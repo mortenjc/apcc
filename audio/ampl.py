corrected normalization, added save to file, and plot from file, rename files

audio/ampl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-# record N samples, performs FFT and plots the
-# spectrum. Normalization is still TBD
+# Record N samples from uSB sounddevice and plot the
+# samples (amplitude) as function of time
 
 import scard, plot, time, argparse, sys
 from collections import namedtuple
@@ -19,16 +19,10 @@ def main(sc):
     sf = sc.input.samplerate
     t = np.arange(sc.input.samples)/sf
 
-    rec = sc.record()
+    rec = sc.record(sc.input.norm)
 
-    sp1 = sc.input.norm*np.fft.rfft(rec[0])/sc.input.samples
-    sp2 = sc.input.norm*np.fft.rfft(rec[1])/sc.input.samples
+    ccplot.plotamp(t, rec, 'a(t)')
 
-    N = len(sp1)
-    n = np.arange(N)
-    f = n/(N/sf)/2
-
-    ccplot.plotfft(f, [np.abs(sp1), np.abs(sp2)])
 
 
 if __name__ == '__main__':
@@ -37,8 +31,6 @@ if __name__ == '__main__':
         help='show list of audio devices and exit')
     parser.add_argument('--device', type=str, default='Scarlett',
         help='device index')
-    parser.add_argument('--channels', type=int, default=1,
-        help='number of channels to record (1 or 2)')
     parser.add_argument('--srate', type=int, default=192000,
         help='number of samples per second')
     parser.add_argument('--samples', type=int, default=192000,
@@ -46,14 +38,16 @@ if __name__ == '__main__':
     parser.add_argument('--norm', type=float, default=1.0,
         help='normalization (to 1V)')
 
-    args, remaining = parser.parse_known_args()
 
+    channels = 2
+
+    args, remaining = parser.parse_known_args()
 
     if args.l:
         scard.query_devices()
         parser.exit(0)
 
-    input = scard.input(args.device, args.srate, args.samples, args.channels, args.norm)
+    input = scard.input(args.device, args.srate, args.samples, channels, args.norm)
     scard = scard.scard(input)
 
     main(scard)